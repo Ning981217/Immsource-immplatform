f4e321e7cac1f268fcb7111da07dee12f0247ebf
王富贵 <91306450+Ning981217@users.noreply.github.com>
2023-03-23 02:18:28 +0800

    Update packets.py

--- immplatform/f1_2018_telemetry/packets.py
+++ immplatform/f1_2018_telemetry/packets.py
@@ -1,24 +1,3 @@
-# uncompyle6 version 3.5.0
-# Python bytecode 3.6 (3379)
-# Decompiled from: Python 2.7.5 (default, Nov 16 2020, 22:23:17) 
-# [GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]
-# Embedded file name: f1_2018_telemetry\packets.py
-# Compiled at: 1995-09-28 00:18:56
-# Size of source mod 2**32: 257 bytes
-"""F1 2018 UDP Telemetry support package
-
-This package is based on the CodeMasters Forum post documenting the F1 2018 packet format:
-
-    https://forums.codemasters.com/topic/38920-f1-2018-udp-specification/
-
-Compared to the definitions given there, the Python version has the following changes:
-
-(1) In the 'PacketMotionData' structure, the comments for the three m_angularAcceleration{X,Y,Z} fields erroneously
-    refer to 'velocity' rather than 'acceleration'. This was corrected.
-(2) In the 'CarSetupData' structure, the comment of the m_rearAntiRollBar refer to rear instead of front. This was corrected.
-(3) In the Driver IDs table, driver 34 has name "Wilheim Kaufmann".
-    This is a typo; whenever this driver is encountered in the game, his name is given as "Wilhelm Kaufmann".
-"""
 import ctypes, enum
 
 class PackedLittleEndianStructure(ctypes.LittleEndianStructure):
